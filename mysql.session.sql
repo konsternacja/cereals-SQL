4904db73a0d9613faee9c2c48de0755dd8255f46
Konstancja <galat.konstancja@gmail.com>
2023-08-15 18:57:15 +0200

    add .gitignore

--- mysql.session.sql
+++ mysql.session.sql
@@ -1,46 +1,32 @@
-SHOW DATABASES;
-
-CREATE DATABASE cereals;
-
 USE cereals;
 
-CREATE TABLE nutrition(
-brand VARCHAR(255) NOT NULL,
-calories INT NOT NULL,
-protein INT NOT NULL,
-fat INT NOT NULL,
-sodium INT NOT NULL,
-fiber INT NOT NULL,
-carbo INT NOT NULL,
-sugars INT NOT NULL,
-potass INT NOT NULL,
-vitamins INT NOT NULL,
-PRIMARY KEY (brand)
+-- Creating table 'nutrition'
+CREATE TABLE IF NOT EXISTS main (
+    brand VARCHAR(255),
+    mfr VARCHAR(1),
+    type VARCHAR(1),
+    calories INT,
+    protein INT,
+    fat INT,
+    sodium INT,
+    fiber FLOAT,
+    carbo FLOAT,
+    sugars INT,
+    potass INT,
+    vitamins INT,
+    shelf INT,
+    weight FLOAT,
+    cups FLOAT,
+    rating FLOAT,
+    PRIMARY KEY (brand)
 );
 
-CREATE TABLE other(
-brand VARCHAR(255) NOT NULL,
-mfr VARCHAR(255) NOT NULL,
-type VARCHAR(255) NOT NULL,
-shelf INT NOT NULL,
-weight FLOAT NOT NULL,
-cups FLOAT NOT NULL,
-rating FLOAT NOT NULL,
-PRIMARY KEY (brand)
-);
-
-USE cereals;
 
 LOAD DATA LOCAL INFILE '/home/konsternacja/Git/cereals-SQL/data/cereal.csv'
-INTO TABLE nutrition
-FIELDS TERMINATED BY ','
-LINES TERMINATED BY '/n'
-IGNORE 1 ROWS
-(brand, mfr, type, calories, protein, fat, sodium, fiber, carbo, sugars, potass, vitamins, shelf, weight, cups, rating);
+INTO TABLE main
+FIELDS TERMINATED BY ',' 
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES;
 
-LOAD DATA LOCAL INFILE '/home/konsternacja/Git/cereals-SQL/data/cereal.csv'
-INTO TABLE nutrition
-FIELDS TERMINATED BY ','
-LINES TERMINATED BY '/n'
-IGNORE 1 ROWS
-(brand, mfr, type, shelf, weight, cups, rating);
+select * from main;
+
